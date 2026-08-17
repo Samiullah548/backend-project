825c66af55748574df2f00eff6a3eb6d3138514f
Samiullah Khan <kgamers748@gmail.com>
2026-08-17 23:41:38 +0530

    add refresh token and access token endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -215,4 +215,4 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 })
 
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken }
+export { registerUser, loginUser, logoutUser, refreshAccessToken } 
